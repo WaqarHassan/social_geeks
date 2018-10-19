9bfb3eddd7f913c75eb756a6141be1e6d0f6f0e3
WaqarHassan <whassan25@gmail.com>
2018-10-19 11:21:33 +0500

    React basic layout setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+
+import NavBar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My React APP</h1>
+        <NavBar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
